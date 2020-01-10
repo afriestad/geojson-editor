de1cc01ad4a3b43da03326078ba328046fef403a
afriestad <afriestad@users.noreply.github.com>
2020-01-10 18:46:47 +0100

    Install react-redux-toastr

--- src/index.js
+++ src/index.js
@@ -2,15 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
+import ReduxToastr from 'react-redux-toastr'
 import store from './store';
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import 'react-redux-toastr/lib/css/react-redux-toastr.min.css';
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
+    <ReduxToastr
+      timeOut={4000}
+      newestOnTop={true}
+      preventDuplicates
+      position="top-right"
+      getState={(state) => state.toastr} // This is the default
+      transitionIn="fadeIn"
+      transitionOut="fadeOut"
+      progressBar
+      closeOnToastrClick/>
   </Provider>,
   document.getElementById('root')
 );
--- src/reducers.js
+++ src/reducers.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux-loop';
 
+import {reducer as toastrReducer} from 'react-redux-toastr';
+
 import fileReaderReducer from './reducers/FileReaderReducers';
 
 const rootReducer = combineReducers({
+  toastr: toastrReducer,
+  
   files: fileReaderReducer,
 });
 
